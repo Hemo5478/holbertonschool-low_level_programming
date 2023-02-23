b1ffa116882f85b54d6fa5a7edbb70172972f29f
Hemo5478 <hamza9638@hotmail.com>
2023-02-23 04:34:21 -0800

    Fixing

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase then uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int y;
+	int a;
+	int b;
+
+	i = 1;
+	y = 1;
+	a = 97;
+	b = 65;
+
+	while (i < 27)
+	{
+		putchar(a);
+		i++;
+		a++;
+	}
+        while (y < 27)
+	{
+		putchar (b);
+		y++;
+		b++;
+	}
+		putchar('\n');
+	return (0);
+}
+
