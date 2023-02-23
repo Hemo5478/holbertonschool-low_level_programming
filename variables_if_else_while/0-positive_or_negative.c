c771c970cc41409752dab6e2f1d941c304beb00d
Hemo5478 <hamza9638@hotmail.com>
2023-02-23 01:07:57 -0800

    Fixing

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - A program that prints whether the number is pos or neg
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if ( n > 0 ) {
+		printf("%d is positive", &n);
+	}
+	else if ( n < 0 ) {
+		printf("%d is negative", &n);
+	}
+	else if ( n == 0 ) {
+		printf("%d is zero", &n);
+	}
+	return 0;
+}
