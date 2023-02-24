ad239866ce9f60783aacfacff9f855583e7714ba
Hemo5478 <hamza9638@hotmail.com>
2023-02-24 03:32:08 -0800

    Fixing

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints reversed alphabet letters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int y;
+
+	i = 1;
+	y = 122;
+
+	while (i < 27)
+	{
+		putchar(y);
+		i++;
+		y--;
+	}
+		putchar('\n');
+	return (0);
+}
