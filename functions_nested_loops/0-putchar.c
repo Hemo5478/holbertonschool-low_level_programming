b80b8778a4ac9e219480654c9d6f3845931f54ad
Hemo5478 <hamza9638@hotmail.com>
2023-02-24 04:59:40 -0800

    Fixing

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("\_putchar\n");
+
+	return (0);
+}
