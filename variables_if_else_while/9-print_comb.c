10db744b41c7ad57dd5e9f072693c51b2bd35035
Hemo5478 <hamza9638@hotmail.com>
2023-02-24 04:11:03 -0800

    Fixing

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints all possible combination of single digit num
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int y;
+
+	i = 1;
+	y = 48;
+
+	while (i < 11)
+	{
+	if (y == 57)
+	{
+		putchar(y);
+	}
+	else
+	{
+		putchar(y);
+		putchar(44);
+		putchar(32);
+		i++;
+		y++;
+	}
+	}
+	return (0);
+}
