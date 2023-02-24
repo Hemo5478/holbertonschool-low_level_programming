57338bfcf791c27e36a9650062080cc78fe165ac
Hemo5478 <hamza9638@hotmail.com>
2023-02-24 03:07:05 -0800

    Fixing

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int y;
+
+	i = 1;
+	y = 48;
+
+	while (i < 11)
+	{
+		putchar(y);
+		i++;
+		y++;
+	}
+		putchar('\n');
+	return (0);
+}
