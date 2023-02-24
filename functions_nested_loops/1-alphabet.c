04ddda8be9f07754a35c8df00c7c7e34d30c1ee9
Hemo5478 <hamza9638@hotmail.com>
2023-02-24 13:12:45 -0800

    Fixing

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,11 +6,12 @@
  * Return: Always 0 (Success)
  */
 
+void print_alphabet(void)
+
 /**
- * print_alphabet - A function that prints the alphabet in lowercase
+ * print_alphabet - A function that prints alphabets in lowercase
  */
 
-void print_alphabet(void)
 {
 	int i;
 	int y;
