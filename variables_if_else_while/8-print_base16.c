2226182be1e23e1689b1614fe599244662ecf7ef
Hemo5478 <hamza9638@hotmail.com>
2023-02-24 03:41:04 -0800

    Fixing

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints all numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int y;
+
+	i = 1;
+	y = 48;
+
+	while (i < 11)
+	{
+		putchar(y);
+		i++;
+		y++;
+	}
+
+	i = 1;
+	y = 97;
+
+	while (i <7)
+	{
+		putchar(y);
+		i++;
+		y++;
+	}
+		putchar('\n');
+	return (0);
+}
