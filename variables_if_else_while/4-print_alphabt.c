8ce21f23caaa07dcc85f935ba2fe75b98140d19a
Hemo5478 <hamza9638@hotmail.com>
2023-02-24 02:18:41 -0800

    Fixing

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints lowercase alphabet without q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int y;
+
+	i = 1;
+	y = 97;
+
+	while (i < 27)
+	{
+	if (y == 101) || (y  == 113)
+	{
+		i++;
+		y++;
+	else
+		putchar(y);
+		i++;
+		y++;
+	}
+	}
+		putchar('\n');
+	return (0);
+}
