6f685cba55ade85804a944971c153ffe4d732824
Hemo5478 <hamza9638@hotmail.com>
2023-03-01 17:59:52 -0800

    Fixing

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - A function that checks for uppercase character
+ * @c: A number
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+		else
+	{
+		return (0);
+	}
+}
