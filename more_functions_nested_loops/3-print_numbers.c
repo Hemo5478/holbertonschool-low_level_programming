1ed180c241bc78f570efe67df90ed29c185ae149
Hemo5478 <hamza9638@hotmail.com>
2023-03-09 01:18:53 -0800

    Fixing

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - A function that prints numbers, from 0 to 9
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_numbers(void)
+{
+	int i;
+	i=0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
