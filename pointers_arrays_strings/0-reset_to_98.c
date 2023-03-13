21ec90f0f7f725676d3450293e80ee8ac6cdebf9
Hemo5478 <hamza9638@hotmail.com>
2023-03-13 05:06:44 -0700

    Fixng

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that takes a pointer and updates the value
+ * @*n: A number
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
