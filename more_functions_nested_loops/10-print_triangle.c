570f75e3456414687e02e16de465d58a21e8d8ea
Hemo5478 <hamza9638@hotmail.com>
2023-03-13 01:42:11 -0700

    Fixing

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - A function that prints a triangle
+ * @size: A number
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_triangle(int size)
+{
+	int j, i, a;
+
+	j = 1;
+	a = size;
+	while (j <= size)
+	{
+		i = 1; 
+		while (i < a)
+		{
+			_putchar(32);
+			i++;
+		}
+		while (i <= size)
+		{
+			_putchar('#');
+			i++;
+		}
+			_putchar('\n');
+		j++;
+		a--;
+	}
+}
