273ee89095198e3ba4914deb832fcaf6a22f1965
Hemo5478 <hamza9638@hotmail.com>
2023-03-13 09:43:50 -0700

    Fixing

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _puts - A function that prints a string , followed by a new line
+ * @str: A string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void _puts(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (str[j] <= i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
