66bca09ac6aee7ed6acf050de8a90cd4bc3f2e56
Hemo5478 <hamza9638@hotmail.com>
2023-03-23 01:14:44 -0700

    Fiing

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -16,7 +16,6 @@ char *_strcpy(char *dest, char *src)
 	while (src[n] != '\0')
 	{
 		n++;
-	}
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
@@ -24,6 +23,7 @@ char *_strcpy(char *dest, char *src)
 	for ( ; i < n ; i++)
 	{
 		dest[i] = '\0';
+	}
 	}
 	return (dest);
 }
