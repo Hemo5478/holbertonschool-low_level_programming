3e96dad7ad3fad91b0a8022b0f9a318de6511efd
Hemo5478 <hamza9638@hotmail.com>
2023-03-27 02:00:36 -0700

    Fixing

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strncat - A function that concatenates two strings
+ * @dest: A string
+ * @src: A string
+ * @n: A number
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, len;
+
+	len = 0;
+	i = 0;
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	while ((i < n) && (src[i] != '\0'))
+	{
+		dest[len + i] = src[i];
+		i++;
+	}
+	dest[n + i] = '\0';
+	return (dest);
+}
