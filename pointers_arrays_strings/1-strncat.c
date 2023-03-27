0e6940f88cd66525338bf073123b5606b3e75867
Hemo5478 <hamza9638@hotmail.com>
2023-03-27 02:57:39 -0700

    Fixing

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -15,11 +15,11 @@ char *_strncat(char *dest, char *src, int n)
 
 	len = 0;
 	i = 0;
-	while (src[len] != '\0')
+	while (dest[len] != '\0')
 	{
 		len++;
 	}
-	while ((i < n) && (src[i] != '\0'))
+	while ((i < len) && (src[i] != '\0'))
 	{
 		dest[len + i] = src[i];
 		i++;
