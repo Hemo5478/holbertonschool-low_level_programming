1a4eb884f0a05c320c75e89c913f2e7fc3c0aa6b
Hemo5478 <hamza9638@hotmail.com>
2023-03-31 03:41:47 -0700

    Fixint

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * *string_toupper - A function that changes all lowercase lettes to uppercase 
+ * *string_toupper - A function that changes all lowercase lettes to upper
+ * * @sr: A string
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -13,15 +14,11 @@ char *string_toupper(char *sr)
 	i = 0;
 	while (sr[i] != '\0')
 	{
-		if ((sr[i] >= 'A') && (sr[i] <= 'Z'))
-		{
-			i++;
-		}
-		else if ((sr[i] >= 'a') && (sr[i] <= 'z'))
+		if ((sr[i] >= 'a') && (sr[i] <= 'z'))
 		{
 			sr[i] = sr[i] - 32;
-			i++;
 		}
+		i++;
 	}
 	return (sr);
 }
