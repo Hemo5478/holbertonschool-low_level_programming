51fa57af3b3217f73669ff59db0c22ae9ecc7922
Hemo5478 <hamza9638@hotmail.com>
2023-03-31 04:23:37 -0700

    Fixing

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *cap_string - A function that capitalizes all words of a string
+ * @sr: A string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *cap_string(char *sr)
+{
+	int i;
+
+	i = 0;
+	while (sr[i] != '\0')
+	{
+		if ((sr[i] == ' ') || (sr[i] == ',') || (sr[i] == '.'))
+		{
+			i++;
+		}
+		if ((sr[i] >= 'a') && (sr[i] <= 'z'))
+		{
+			sr[i] = sr[i] - 32;
+		}
+		else
+		{
+			if ((sr[i] >= 'A') && (sr[i] <= 'Z'))
+			{
+				sr[i] = sr[i] + 32;
+			}
+		}
+		i++;
+	}
+	return (sr);
+}
