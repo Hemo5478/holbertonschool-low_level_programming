fe98945dac782f4301997143002230f70cb21529
Hemo5478 <hamza9638@hotmail.com>
2023-03-31 02:20:11 -0700

    Fixing

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_reverse_array - A function that reverses the content of an array of integers
+ * @a: An array of integers
+ * @n: The number of elements to swap
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, p;
+
+	i = 0;
+	j = n - 1;
+	while (i < (n / 2))
+	{
+		p = a[i];
+		a[i] = a[j];
+		a[j] = p;
+		i++;
+		j--;
+	}
+}
