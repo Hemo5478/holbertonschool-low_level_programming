1a2a35144b1497a91433c8e0152048829b1179cf
Hemo5478 <hamza9638@hotmail.com>
2023-04-04 03:30:58 -0700

    Fixing

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,19 +11,19 @@
 char *_strchr(char *s, char c)
 {
 	int i;
+
 	i = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
 			return(s + i);
-			break;
 		}
 		i++;
 	}
 	if (s[i] == '\0')
 	{
-		return (0);
+		return ('\0');
 	}
 	return(0);
 }
