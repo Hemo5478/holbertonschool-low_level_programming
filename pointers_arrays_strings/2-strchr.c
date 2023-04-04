cbf1c78cb200a9e55631b6844aad7575271518e9
Hemo5478 <hamza9638@hotmail.com>
2023-04-04 03:28:26 -0700

    Fixing

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strchr - A function that locates a character in a string
+ * @s: A string
+ * @c: A character
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return(s + i);
+			break;
+		}
+		i++;
+	}
+	if (s[i] == '\0')
+	{
+		return (0);
+	}
+	return(0);
+}
