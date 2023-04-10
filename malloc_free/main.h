1a5d20321e98ddd2f6d2e8da4567506d860f5ace
Hemo5478 <hamza9638@hotmail.com>
2023-04-10 01:56:14 -0700

    Fixing

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -44,3 +44,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void _puts_recursion(char *s);
+char *create_array(unsigned int size, char c);
