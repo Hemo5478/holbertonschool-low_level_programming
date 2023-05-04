337e001dc6507efa9aa74acfb17efc972944e7f7
Hemo5478 <hamza9638@hotmail.com>
2023-05-04 07:23:48 -0700

    Fixing

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,23 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct dt - structure defined to hold a single character
+ * @letter: A letter for data type
+ * @func: A function pointer
+ */
+
+typedef struct dt
+{
+	char letter;
+	void (*func)(va_list);
+} datatype;
 
 #endif
