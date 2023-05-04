fd021e48213f5f171146fc88d4baf3e1cfe859b2
Hemo5478 <hamza9638@hotmail.com>
2023-05-03 17:34:49 -0700

    Fixing

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -6,24 +6,36 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _sqrt_recursion(int n)
 {
-	int a;
-	if (n == 0 || n == 1)
-		return (n);
-	a = 1;
-	while (a * a <= n)
-	{
-		a++;
-	}
-	a = a - 1;
-	if (a * a == n)
-	{
-		return (a);
-	}
-	else
+	if (n < 0)
 	{
 		return (-1);
 	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	return (_sqrt_repeater(n, 1));
+}
+
+/**
+ * _sqrt_repeater - Finds the natural square root
+ * @n: A number
+ * @i: A guesser
+*  Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _sqrt_repeater(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	i++;
+	return (_sqrt_repeater(n, i));
 }
