f0b94e57e5d0907026cb71490619befde0cd5173
Hemo5478 <hamza9638@hotmail.com>
2023-05-04 13:22:37 -0700

    Fixing

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -13,7 +13,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current, *tmp;
 	unsigned int y;
 
-	if (!*head)
+	if (!head || !(*head))
 		return (-1);
 
 	current = *head;
