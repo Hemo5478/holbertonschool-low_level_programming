fe205b71d0bcc1655cdc1b2e57e44c2af579c3bf
Hemo5478 <hamza9638@hotmail.com>
2023-05-04 04:59:59 -0700

    Fixing

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,6 +1,13 @@
 #ifndef CALC_H
 #define CALC_H
 
+/**
+ * struct op - A structure that define an operator
+ *
+ * @op: The operator
+ * @f: The associated function
+ */
+
 typedef struct op
 {
 char *op;
