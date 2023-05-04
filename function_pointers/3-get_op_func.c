819fbb1ed28fe9d37ffaa4bbec43993aa52fa20c
Hemo5478 <hamza9638@hotmail.com>
2023-05-04 04:59:41 -0700

    Fixing

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -2,7 +2,7 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - selects the correct function to perform the operation 
+ * get_op_func - selects the correct function to perform the operation
  * @s: operation given (+, *, -, /, %)
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -10,7 +10,8 @@
 
 int (*get_op_func(char *s))(int a, int b)
 {
-  int y;
+	int y;
+
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
