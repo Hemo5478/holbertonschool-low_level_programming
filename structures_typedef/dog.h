62ece992fb93e1d6a0e2bd38761ab61e14ef689d
Hemo5478 <hamza9638@hotmail.com>
2023-05-04 02:00:56 -0700

    Fixing

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure for the dogs
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner:The dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
