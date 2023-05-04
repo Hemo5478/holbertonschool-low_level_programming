3da039fea1d5da0531185fcaeb2732afe7c1abf4
Hemo5478 <hamza9638@hotmail.com>
2023-05-04 11:59:34 -0700

    Fixing

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,66 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strlen - A function that returns the length of a string
+ * @s: A string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node_end - A function that adds a new node at the end
+ * @head: A pointer to a pointer
+ * @str: A string
+ * Return: The address of the new element, or NULL if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *temp;
+
+	if (!str)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = _strlen(new_node->str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new_node;
+	}
+
+	return (new_node);
+}
