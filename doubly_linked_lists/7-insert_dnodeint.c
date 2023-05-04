9ea94013a9b4f76580c9bb57f0c3be2532eb8ad0
Hemo5478 <hamza9638@hotmail.com>
2023-05-04 13:07:50 -0700

    Fixing

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,14 +18,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	if (h == NULL)
+	if (!h)
 		return (NULL);
 
 	current = *h;
 	y = 0;
 	while (y < idx - 1)
 	{
-		if (current)
+		if (current == NULL)
 			return (NULL);
 		current = current->next;
 		y++;
