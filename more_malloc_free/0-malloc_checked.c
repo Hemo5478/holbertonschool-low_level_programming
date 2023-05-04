406075016cbbf022a2cc3477fef4d017a4552d5e
Hemo5478 <hamza9638@hotmail.com>
2023-05-03 20:37:35 -0700

    Fixing

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - A function that allocates memory using malloc 
+ * *@b: The size of the memory block
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;
+
+	pointer = malloc(b);
+	if (pointer == NULL)
+	{
+		exit(98);
+	}
+	return (pointer);
+}
