f7ce11c1edf4566c73fac0359c208c5bd6914fb0
Hemo5478 <hamza9638@hotmail.com>
2023-05-04 04:17:03 -0700

    Fixing

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - A function that searches for an integer.
+ * @array: A pointer to an array of integers
+ * @size: The number of elements in the array
+ * @cmp: A pointer to function
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int y;
+	if (!array || !cmp || size <= 0)
+	{
+		return (-1);
+	}
+
+	y = 0;
+	do {
+		if (cmp(array[y]) != 0)
+		{
+			return (y);
+		}
+		y++;
+	} while (y < size);
+
+	return (-1);
+}
