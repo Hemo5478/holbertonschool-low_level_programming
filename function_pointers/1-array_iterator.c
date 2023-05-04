403cc8dc828f6425d4b26434a0276b4adb5d49b6
Hemo5478 <hamza9638@hotmail.com>
2023-05-04 03:59:51 -0700

    Fixing

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - A function that executes a function
+ * @array: An array of integers
+ * @size: The size of the array
+ * @action: A pointer to function
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && action)
+	{
+		size_t y = 0;
+		while (y < size)
+		{
+			action(array[y]);
+			y++;
+		}
+	}
+}
