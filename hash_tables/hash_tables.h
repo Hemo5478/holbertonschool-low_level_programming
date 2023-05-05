e60ac210c6ab444d8e0842e1bceca196f3b53cd0
Hemo5478 <hamza9638@hotmail.com>
2023-05-04 17:50:27 -0700

    Fixing

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -5,6 +5,26 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array
+ */
+
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
 hash_table_t *hash_table_create(unsigned long int size);
 
 #endif
